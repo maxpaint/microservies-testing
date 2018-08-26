45e6c56429ab06f1512c197934e49db2e9a1a585
maxdanylenko <danylenko.max@gmail.com>
2018-08-26 14:30:25 +0300

    Init project

---
+++ init/src/test/java/com/intellias/advisory/greeting/api/PersonControllerTest.java
@@ -0,0 +1,69 @@
+package com.intellias.advisory.greeting.api;
+
+import com.intellias.advisory.greeting.api.vo.GreetingVO;
+import com.intellias.advisory.greeting.config.GreetingConfig;
+import com.intellias.advisory.greeting.jpa.model.Person;
+import com.intellias.advisory.greeting.jpa.repository.GreetingMessageRepository;
+import com.intellias.advisory.greeting.jpa.repository.PersonRepository;
+import com.intellias.advisory.greeting.util.FakerUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class PersonControllerTest {
+
+    private PersonController personController;
+
+    private GreetingConfig greetingConfig = new GreetingConfig();
+
+    @Mock
+    private GreetingMessageRepository greetingMessageRepository;
+
+    @Mock
+    private PersonRepository personRepo;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        ReflectionTestUtils.setField(greetingConfig, "greeting", "Hello");
+        personController = new PersonController(greetingConfig, personRepo, greetingMessageRepository);
+    }
+
+    @Test
+    public void shouldReturnGreetingVOForPerson() {
+        Person person = Person.builder()
+                .email(FakerUtil.getEmail())
+                .firstName(FakerUtil.getFirstName())
+                .lastName(FakerUtil.getLastName())
+                .build();
+        given(personRepo.findByEmail(person.getEmail()))
+                .willReturn(Optional.of(person));
+
+        GreetingVO greeting = personController.greeting(person.getEmail());
+
+        assertThat(greeting.getFirstName(), is(person.getFirstName()));
+        assertThat(greeting.getSecondName(), is(person.getLastName()));
+        assertThat(greeting.getMessage(), is("Hello"));
+    }
+
+    @Test
+    public void shouldReturnEmptyGreetingVOForPerson() {
+        given(personRepo.findByEmail(anyString()))
+                .willReturn(Optional.empty());
+
+        String email = FakerUtil.getEmail();
+        GreetingVO greeting = personController.greeting(email);
+
+        assertThat(greeting.getMessage(), is(format("Who is this '%s' ? Please register in the service", email)));
+    }
+}
